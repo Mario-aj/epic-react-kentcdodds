574761acf2053c3309fcc62d1fffd5e8838750d8
Mario-aj <mariojorge1997@mail.ru>
2021-11-11 09:40:27 +0100

    use useEffect hook for HTTP request

--- react-hooks/src/__tests__/06.js
+++ react-hooks/src/__tests__/06.js
@@ -2,38 +2,50 @@ import * as React from 'react'
 import {alfredTip} from '@kentcdodds/react-workshop-app/test-utils'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/06'
-// import App from '../exercise/06'
+// import App from '../final/06'
+import App from '../exercise/06'
 
 beforeEach(() => jest.spyOn(window, 'fetch'))
 afterEach(() => window.fetch.mockRestore())
 
-test('displays the pokemon', async () => {
-  render(<App />)
-  const input = screen.getByLabelText(/pokemon/i)
-  const submit = screen.getByText(/^submit$/i)
+describe(App, () => {
+  it('should render correctly', () => {
+    render(<App />)
 
-  // verify that an initial request is made when mounted
-  userEvent.type(input, 'pikachu')
-  userEvent.click(submit)
+    expect(screen.getByText(/Pokemon Name/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('textbox', {name: /pokemon name/i}),
+    ).toBeInTheDocument()
+    expect(screen.getByRole('button', {name: /submit/i})).toBeInTheDocument()
+    expect(screen.getByText(/Submit a pokemon/i)).toBeInTheDocument()
+  })
+  it('displays the pokemon', async () => {
+    render(<App />)
+    const input = screen.getByLabelText(/pokemon/i)
+    const submit = screen.getByText(/^submit$/i)
 
-  await screen.findByRole('heading', {name: /pikachu/i})
+    // verify that an initial request is made when mounted
+    userEvent.type(input, 'pikachu')
+    userEvent.click(submit)
 
-  // verify that a request is made when props change
-  userEvent.clear(input)
-  userEvent.type(input, 'ditto')
-  userEvent.click(submit)
+    await screen.findByRole('heading', {name: /pikachu/i})
 
-  await screen.findByRole('heading', {name: /ditto/i})
+    // verify that a request is made when props change
+    userEvent.clear(input)
+    userEvent.type(input, 'ditto')
+    userEvent.click(submit)
 
-  // verify that when props remain the same a request is not made
-  window.fetch.mockClear()
-  userEvent.click(submit)
+    await screen.findByRole('heading', {name: /ditto/i})
 
-  await screen.findByRole('heading', {name: /ditto/i})
+    // verify that when props remain the same a request is not made
+    window.fetch.mockClear()
+    userEvent.click(submit)
 
-  alfredTip(
-    () => expect(window.fetch).not.toHaveBeenCalled(),
-    'Make certain that you are providing a dependencies list in useEffect.',
-  )
+    await screen.findByRole('heading', {name: /ditto/i})
+
+    alfredTip(
+      () => expect(window.fetch).not.toHaveBeenCalled(),
+      'Make certain that you are providing a dependencies list in useEffect.',
+    )
+  })
 })
