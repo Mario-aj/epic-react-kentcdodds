a99b44d78d7354592dab744fad476e7bfa10048b
mario-aj <mariojorge1997@mail.ru>
2021-11-18 08:02:14 +0100

    learn useReducer hook

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -3,16 +3,19 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return state + action.step
+    default:
+      throw new Error()
+  }
+}
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+function Counter({initialCount = 0, step = 1}) {
+  const [count, setCount] = React.useReducer(reducer, initialCount)
+
+  const increment = () => setCount({type: 'increment', step})
   return <button onClick={increment}>{count}</button>
 }
 
