c1338c158b8afd058c82d22a3d80422926e6c708
mario-aj <mariojorge1997@mail.ru>
2021-11-18 08:57:58 +0100

    pass single value as the action to dispatch reducer function

--- advanced-react-hooks/src/exercise/01.extra-01.js
+++ advanced-react-hooks/src/exercise/01.extra-01.js
@@ -4,13 +4,13 @@
 import * as React from 'react'
 
 function reducer(state, action) {
-  return action
+  return state + action
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(reducer, initialCount)
+  const [count, changeCount] = React.useReducer(reducer, initialCount)
 
-  const increment = () => setCount(count + step)
+  const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
