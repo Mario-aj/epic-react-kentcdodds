562390b1ab40688740eedf7c85e69e40c2a093b8
mario-aj <mariojorge1997@mail.ru>
2021-12-03 12:03:24 +0100

    learnin useLayoutEffect

--- advanced-react-hooks/src/exercise/04.js
+++ advanced-react-hooks/src/exercise/04.js
@@ -5,8 +5,8 @@ import * as React from 'react'
 
 function MessagesDisplay({messages}) {
   const containerRef = React.useRef()
-  // 🐨 replace useEffect with useLayoutEffect
-  React.useEffect(() => {
+
+  React.useLayoutEffect(() => {
     containerRef.current.scrollTop = containerRef.current.scrollHeight
   })
 
@@ -33,7 +33,7 @@ function SlooooowSibling() {
   // how it impacts interactivity of the page before updates.
   React.useEffect(() => {
     // increase this number to see a more stark difference
-    sleep(300)
+    sleep(600)
   })
   return null
 }
