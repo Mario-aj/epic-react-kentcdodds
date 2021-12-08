c6a99b7d66bc8256d879b9da096ea11af3abeb99
mario-aj <mariojorge1997@mail.ru>
2021-12-08 09:40:55 +0100

    make our component to accept DOM component

---
+++ advanced-react-patterns/src/exercise/02.extra-01.js
@@ -0,0 +1,40 @@
+// Compound Components
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function Toggle({children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return React.Children.map(children, (child, index) => {
+    if (typeof child.type === 'string') return child
+    return React.cloneElement(child, {
+      key: `child-${index}`,
+      on,
+      toggle,
+    })
+  })
+}
+
+const ToggleOn = ({on, children}) => on && children
+
+const ToggleOff = ({on, children}) => !on && children
+
+const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
+
+function App() {
+  return (
+    <div>
+      <Toggle>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
+        <ToggleButton />
+      </Toggle>
+    </div>
+  )
+}
+
+export default App
