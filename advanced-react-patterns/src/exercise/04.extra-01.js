e379e82b13d3f6083772d7a7d8751803d20b3336
mario-aj <mariojorge1997@mail.ru>
2022-01-06 09:09:22 +0100

    solve prop getters problems

---
+++ advanced-react-patterns/src/exercise/04.extra-01.js
@@ -0,0 +1,46 @@
+// Prop Collections and Getters
+// http://localhost:3000/isolated/exercise/04.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+function useToggle() {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  function getTogglerProps(props) {
+    const {on, onClick, ...rest} = props
+
+    if (onClick) onClick()
+
+    return {on, 'aria-pressed': on, onClick: toggle, ...rest}
+  }
+
+  return {on, toggle, getTogglerProps}
+}
+
+function App() {
+  const {on, getTogglerProps} = useToggle()
+  return (
+    <div>
+      <Switch {...getTogglerProps({on})} />
+      <hr />
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
+        {on ? 'on' : 'off'}
+      </button>
+    </div>
+  )
+}
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
